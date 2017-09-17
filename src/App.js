bc29ab2c7d506a18e7d837775f74ae17d83c60d4
Matthew Poirier <matthew@sitedocs.com>
2017-09-17 18:21:45 -0300

    more fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import { debounce } from 'lodash'
 
 import { HotKeys } from 'react-hotkeys'
@@ -15,19 +16,20 @@ const numImages = 7
 
 const map = {
   'moveLeft': 'left',
-  'moveRight': 'right'
+  'moveRight': 'right',
+  'download': 'down'
 };
 
 class App extends Component {
   constructor(props) {
     const visited = window.localStorage.getItem('visited')
+    window.localStorage.setItem('visited', true)
 
     super(props)
 
     this.showButtons = debounce(this.showButtons, 500, { leading: true })
     this.selectImage = debounce(this.selectImage, 500, { leading: true })
 
-    this.wrapperDiv = null
     this.timer = null
     this.state = {
       cur: 0,
@@ -37,21 +39,12 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.timer = setTimeout(this.hideButtons, 5000)
-    this.wrapperDiv.focus()
+    this.timer = setTimeout(this.hideButtons, 3000)
   }
   componentWillUnmount() {
     clearTimeout(this.timer)
   }
 
-  clearSticker = () => {
-    console.log('hey there')
-    window.localStorage.setItem('visited', true)
-
-    this.setState({
-      visited: true
-    })
-  }
   selectImage = (dir) => {
     const { cur } = this.state
 
@@ -84,17 +77,24 @@ class App extends Component {
       right: e => { e.preventDefault(); this.selectImage(1) }
     }
 
+    const autofocus = (el) => {
+      const found = ReactDOM.findDOMNode(el)
+      if (found) {
+        found.focus()
+      }
+    }
+
     return (
-      <HotKeys keyMap={map} handlers={handlers}>
-        <div ref={(ele) => { this.wrapperDiv = ele }} className="App" onMouseMove={this.showButtons}>
+      <HotKeys ref={autofocus} keyMap={map} handlers={handlers}>
+        <div className="App" onMouseMove={this.showButtons}>
           <Header timedOut={timedOut} />
           <Buttons cur={cur} selectImage={this.selectImage} timedOut={timedOut} />
           <BigImage cur={cur} />
-          { visited ? null : <div onClick={this.clearSticker}><Sticker /></div> }
+          { visited ? null : <Sticker /> }
         </div>
       </HotKeys>
     )
   }
 }
-// images={Images}
+//
 export default App
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react'
 import _ from 'lodash'
 
+import { HotKeys } from 'react-hotkeys'
+
 import './Buttons.css'
 
 class Buttons extends Component {
+  forceDownload = () => {
+    this.refs.downloadButton.click()
+  }
+
   render() {
     const { selectImage, cur, timedOut } = this.props
 
     const extraClasses = timedOut ? "Buttons-button-hidden" : ""
 
+    const handlers = {
+      'download': this.forceDownload
+    }
+
     return (
-      <div className="Buttons">
-        <div className={`Buttons-button Buttons-left ${extraClasses}`}>
-          <button onClick={() => selectImage(-1)}></button>
+      <HotKeys handlers={handlers}>
+        <div className="Buttons">
+          <div className={`Buttons-button Buttons-left ${extraClasses}`}>
+            <button onClick={() => selectImage(-1)}></button>
+          </div>
+          <div className={`Buttons-button Buttons-right ${extraClasses}`}>
+            <button onClick={() => selectImage(1)}></button>
+          </div>
+          <div className={`Buttons-button Buttons-download ${extraClasses}`}>
+            <a ref="downloadButton" href={`/images/DeskToppings${cur + 1}.jpg`} target="_blank" download></a>
+          </div>
         </div>
-        <div className={`Buttons-button Buttons-right ${extraClasses}`}>
-          <button onClick={() => selectImage(1)}></button>
-        </div>
-        <div className={`Buttons-button Buttons-download ${extraClasses}`}>
-          <a href={`/images/DeskToppings${cur + 1}.jpg`} target="_blank" download></a>
-        </div>
-      </div>
+      </HotKeys>
     )
   }
 }
